2d764fc85e8f99a3cbc8c3b776f24566f904771b
Fiona <rachel.smith@cfa.harvard.edu>
2021-10-12 15:43:13 -1000

    plots of weather data and offset from median for each ant

--- the_weather.py
+++ the_weather.py
@@ -17,7 +17,17 @@ h_offsets_6 = h_data[:,5]-h_median
 h_offsets_7 = h_data[:,6]-h_median
 h_offsets_8 = h_data[:,7]-h_median
 
-#plt.plot(h_time,h_median,label='median',alpha=.5)
+
+for i in range(0,8):
+    plt.plot(h_time,h_data[:,i],label=str(i+1),lw=0.5)
+plt.plot(h_time,h_median,label='median',lw=.5,alpha=.5,color='blue')
+plt.legend(fontsize='x-small')
+plt.xlim(1633406760,1633449840)          #start time ~6pm, unixtime of ~6am
+plt.ylim(0,120)
+plt.title("Humidity")
+plt.show()
+
+plt.plot(h_time,h_median,label='median',alpha=.5)
 plt.plot(h_time,h_offsets_1,label='ant1',lw=0.5)
 plt.plot(h_time,h_offsets_2,label='ant2',lw=0.5)
 #plt.plot(h_time,h_offsets_3,label='ant3',lw=0.5)
@@ -26,11 +36,88 @@ plt.plot(h_time,h_offsets_5,label='ant5',lw=0.5)
 plt.plot(h_time,h_offsets_6,label='ant6',lw=0.5)
 plt.plot(h_time,h_offsets_7,label='ant7',lw=0.5)
 plt.plot(h_time,h_offsets_8,label='ant8',lw=0.5)
-plt.legend()
-plt.xlim(right=1633449840)          #unixtime of ~6am
+plt.legend(fontsize='xx-small')
+plt.xlim(1633406760,1633449840)          #start time ~6pm, unixtime of ~6am
 plt.ylim(-20,20)
 plt.title("Humidity Offsets")
 plt.show()
+
+
+t_data = np.loadtxt("temp_24hr.txt", usecols=(3,4,5,6,7,8,9,10),skiprows=8)
+t_time = np.loadtxt("temp_24hr.txt", usecols=0,skiprows=8)
+
+t_median = np.median(t_data,axis=1)
+t_offsets_1 = t_data[:,0]-t_median
+t_offsets_2 = t_data[:,1]-t_median
+t_offsets_3 = t_data[:,2]-t_median
+t_offsets_4 = t_data[:,3]-t_median
+t_offsets_5 = t_data[:,4]-t_median
+t_offsets_6 = t_data[:,5]-t_median
+t_offsets_7 = t_data[:,6]-t_median
+t_offsets_8 = t_data[:,7]-t_median
+
+for i in range(0,8):
+    plt.plot(t_time,t_data[:,i],label=str(i+1),lw=0.5)
+plt.plot(t_time,t_median,label='median',lw=.5,alpha=.5,color='blue')
+plt.legend(fontsize='xx-small')
+plt.xlim(1633406760,1633449840)          #start time ~6pm, unixtime of ~6am
+#plt.ylim(0,120)
+plt.title("Temperature")
+plt.show()
+
+plt.plot(t_time,t_median,label='median',alpha=.5)
+plt.plot(t_time,t_offsets_1,label='ant1',lw=0.5)
+plt.plot(t_time,t_offsets_2,label='ant2',lw=0.5)
+#plt.plot(t_time,t_offsets_3,label='ant3',lw=0.5)
+#plt.plot(t_time,t_offsets_4,label='ant4',lw=0.5)
+plt.plot(t_time,t_offsets_5,label='ant5',lw=0.5)
+plt.plot(t_time,t_offsets_6,label='ant6',lw=0.5)
+plt.plot(t_time,t_offsets_7,label='ant7',lw=0.5)
+plt.plot(t_time,t_offsets_8,label='ant8',lw=0.5)
+plt.legend(fontsize='xx-small')
+plt.xlim(1633406760,1633449840)          #start time ~6pm, unixtime of ~6am
+#plt.ylim(-20,20)
+plt.title("Temperature Offsets")
+plt.show()
+
+
+p_data = np.loadtxt("pressure_24hr.txt", usecols=(3,4,5,6,7,8,9,10),skiprows=8)
+p_time = np.loadtxt("pressure_24hr.txt", usecols=0,skiprows=8)
+
+p_median = np.median(p_data,axis=1)
+p_offsets_1 = p_data[:,0]-p_median
+p_offsets_2 = p_data[:,1]-p_median
+p_offsets_3 = p_data[:,2]-p_median
+p_offsets_4 = p_data[:,3]-p_median
+p_offsets_5 = p_data[:,4]-p_median
+p_offsets_6 = p_data[:,5]-p_median
+p_offsets_7 = p_data[:,6]-p_median
+p_offsets_8 = p_data[:,7]-p_median
+
+for i in range(0,8):
+    plt.plot(p_time,p_data[:,i],label=str(i+1),lw=0.5)
+plt.plot(p_time,p_median,label='median',lw=.5,alpha=.5,color='blue')
+plt.legend(fontsize='xx-small',loc='lower right')
+plt.xlim(1633406760,1633449840)          #start time ~6pm, unixtime of ~6am
+plt.ylim(615,630)
+plt.title("Pressure")
+plt.show()
+
+#plt.plot(p_time,p_median,label='median',alpha=.5)
+plt.plot(p_time,p_offsets_1,label='ant1',lw=0.5)
+plt.plot(p_time,p_offsets_2,label='ant2',lw=0.5)
+#plt.plot(p_time,p_offsets_3,label='ant3',lw=0.5)
+#plt.plot(p_time,p_offsets_4,label='ant4',lw=0.5)
+plt.plot(p_time,p_offsets_5,label='ant5',lw=0.5)
+plt.plot(p_time,p_offsets_6,label='ant6',lw=0.5)
+plt.plot(p_time,p_offsets_7,label='ant7',lw=0.5)
+plt.plot(p_time,p_offsets_8,label='ant8',lw=0.5)
+plt.legend(fontsize='xx-small',loc='center right')
+plt.xlim(1633406760,1633449840)          #start time ~6pm, unixtime of ~6am
+#plt.ylim(20,20)
+plt.title("Pressure Offsets")
+plt.show()
+
 
 '''
 #get the data
